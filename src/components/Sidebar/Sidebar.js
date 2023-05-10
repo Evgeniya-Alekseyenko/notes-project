3385baa51df680b2cb3a7744274d5d0bc0811527
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-05-10 16:21:59 +0300

    Added note sorting, the last added note will be displayed on top

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,38 +5,54 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { Typography } from '@mui/material';
 
-export default function Sidebar({ notes, onDeleteNote }) {
+export default function Sidebar({ notes, activeNote, setActiveNote }) {
+    const sortedNotes = notes
+        .slice()
+        .sort((a, b) => b.lastModified - a.lastModified);
+
     return (
         <Box sx={{ overflow: 'auto' }}>
             <List>
-                {notes.map((note, index) => (
+                {sortedNotes.map((note, index) => (
                     <React.Fragment key={index}>
-                        <ListItem disablePadding>
+                        <ListItem
+                            disablePadding
+                            className={`${note.id === activeNote && 'active'}`}
+                            onClick={() => setActiveNote(note.id)}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'flex-start',
+                                alignItems: 'flex-start',
+                            }}
+                        >
                             <ListItemButton>
                                 <ListItemText primary={note.title} />
-                                <div>
-                                    <strong>{note.title}</strong>
-                                    <button
-                                        onClick={() => onDeleteNote(note.id)}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
                             </ListItemButton>
+                            <div>
+                                <p>
+                                    {note.body &&
+                                        note.body.substr(0, 100) + '...'}
+                                </p>
+                                <Typography
+                                    fontSize='12px'
+                                    color='text.secondary'
+                                    fontWeight='bold'
+                                    sx={{ paddingLeft: '6px' }}
+                                >
+                                    Last modified{' '}
+                                    {new Date(
+                                        note.lastModified
+                                    ).toLocaleDateString('en-GB', {
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                    })}
+                                </Typography>
+                            </div>
                         </ListItem>
                         {index < notes.length - 1 && <Divider />}
-                        <p>{note.body && note.body.substr(0, 100) + '...'}</p>
-                        <small>
-                            Last modified{' '}
-                            {new Date(note.lastModified).toLocaleDateString(
-                                'en-GB',
-                                {
-                                    hour: '2-digit',
-                                    minute: '2-digit',
-                                }
-                            )}
-                        </small>
                     </React.Fragment>
                 ))}
             </List>
