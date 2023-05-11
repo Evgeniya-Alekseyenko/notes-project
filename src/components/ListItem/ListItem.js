7de8738493321c9ecefd77acdf2345f9a15da46d
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-05-11 16:18:29 +0300

    Added inactive status to note fields if edit button is not pressed

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,7 +3,29 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
 
-export default function ListItem(activeNote) {
+export default function ListItem({
+    activeNote,
+    onUpdateNote,
+    saveNote,
+    isEditing,
+    // onEditNote,
+}) {
+    // const [isEditing, setIsEditing] = useState(false);
+
+    const onEditField = (field, value) => {
+        const updatedNote = {
+            ...activeNote,
+            [field]: value,
+            lastModified: Date.now(),
+        };
+        onUpdateNote(updatedNote);
+        saveNote(updatedNote);
+    };
+
+    // const onToggleEditMode = () => {
+    //     setIsEditing(!isEditing);
+    // };
+
     if (!activeNote)
         return (
             <Typography
@@ -36,9 +58,12 @@ export default function ListItem(activeNote) {
                     label='Write a note title'
                     id='fullWidth'
                     value={activeNote.title}
+                    onChange={(e) => onEditField('title', e.target.value)}
                     variant='outlined'
                     color='success'
                     sx={{ width: '30vw', marginTop: '20px' }}
+                    disabled={!isEditing}
+                    // disabled={!activeNote.isEditing}
                 />
             </Box>
             <Box
@@ -58,11 +83,17 @@ export default function ListItem(activeNote) {
                     id='body'
                     placeholder='Write your note...'
                     value={activeNote.body}
+                    onChange={(e) => onEditField('body', e.target.value)}
                     variant='outlined'
                     color='success'
                     sx={{ width: '30vw', marginTop: '20px' }}
+                    disabled={!isEditing}
+                    // disabled={!activeNote.isEditing}
                 />
             </Box>
+            {/* <Button onClick={onToggleEditMode}>
+                {isEditing ? 'Save' : 'Edit'}
+            </Button> */}
             <h2>Preview</h2>
             <h1>{activeNote.title}</h1>
             <ReactMarkdown className='markdown-preview'>
