c7e0f6c5b3ba1dab927361eb2a4955ab270869c9
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-05-12 12:54:19 +0300

    Changed the logic of displaying notes depending on their status (saved or edited note)

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,17 +1,12 @@
+import { Box, TextField, Typography } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Typography } from '@mui/material';
 
 export default function ListItem({
     activeNote,
     onUpdateNote,
     saveNote,
     isEditing,
-    // onEditNote,
 }) {
-    // const [isEditing, setIsEditing] = useState(false);
-
     const onEditField = (field, value) => {
         const updatedNote = {
             ...activeNote,
@@ -21,10 +16,6 @@ export default function ListItem({
         onUpdateNote(updatedNote);
         saveNote(updatedNote);
     };
-
-    // const onToggleEditMode = () => {
-    //     setIsEditing(!isEditing);
-    // };
 
     if (!activeNote)
         return (
@@ -41,64 +32,68 @@ export default function ListItem({
 
     return (
         <div>
-            <Box
-                sx={{
-                    width: 500,
-                    maxWidth: '100%',
-                    '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#3CB371',
-                    },
-                    '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#3CB371',
-                    },
-                }}
-            >
-                <TextField
-                    fullWidth
-                    label='Write a note title'
-                    id='fullWidth'
-                    value={activeNote.title}
-                    onChange={(e) => onEditField('title', e.target.value)}
-                    variant='outlined'
-                    color='success'
-                    sx={{ width: '30vw', marginTop: '20px' }}
-                    disabled={!isEditing}
-                    // disabled={!activeNote.isEditing}
-                />
-            </Box>
-            <Box
-                sx={{
-                    maxWidth: '100%',
-                    '&:hover .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#3CB371',
-                    },
-                    '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
-                        borderColor: '#3CB371',
-                    },
-                }}
-            >
-                <TextField
-                    multiline
-                    rows={5}
-                    id='body'
-                    placeholder='Write your note...'
-                    value={activeNote.body}
-                    onChange={(e) => onEditField('body', e.target.value)}
-                    variant='outlined'
-                    color='success'
-                    sx={{ width: '30vw', marginTop: '20px' }}
-                    disabled={!isEditing}
-                    // disabled={!activeNote.isEditing}
-                />
-            </Box>
-            {/* <Button onClick={onToggleEditMode}>
-                {isEditing ? 'Save' : 'Edit'}
-            </Button> */}
-            <h2>Preview</h2>
-            <h1>{activeNote.title}</h1>
-            <ReactMarkdown className='markdown-preview'>
-                {activeNote.body}
-            </ReactMarkdown>
+            {!isEditing && (
+                <>
+                    <h1>{activeNote.title}</h1>
+                    <ReactMarkdown className='markdown-preview'>
+                        {activeNote.body}
+                    </ReactMarkdown>
+                </>
+            )}
+            {isEditing && (
+                <>
+                    <Box
+                        sx={{
+                            width: 500,
+                            maxWidth: '100%',
+                            '&:hover .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#3CB371',
+                            },
+                            '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#3CB371',
+                            },
+                        }}
+                    >
+                        <TextField
+                            fullWidth
+                            label='Write a note title'
+                            id='fullWidth'
+                            value={activeNote.title}
+                            onChange={(e) =>
+                                onEditField('title', e.target.value)
+                            }
+                            variant='outlined'
+                            color='success'
+                            sx={{ width: '30vw', marginTop: '20px' }}
+                        />
+                    </Box>
+                    <Box
+                        sx={{
+                            maxWidth: '100%',
+                            '&:hover .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#3CB371',
+                            },
+                            '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#3CB371',
+                            },
+                        }}
+                    >
+                        <TextField
+                            multiline
+                            rows={5}
+                            id='body'
+                            placeholder='Write your note...'
+                            value={activeNote.body}
+                            onChange={(e) =>
+                                onEditField('body', e.target.value)
+                            }
+                            variant='outlined'
+                            color='success'
+                            sx={{ width: '30vw', marginTop: '20px' }}
+                        />
+                    </Box>
+                </>
+            )}
         </div>
     );
 }
