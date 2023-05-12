93d800b9ba27d3b23e0ee3f3e1b1a673547d2807
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2023-05-12 13:59:41 +0300

    Added logic for interacting with IndexedDB

--- src/components/Workspace/Workspace.js
+++ src/components/Workspace/Workspace.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import SearchBox from '../SearchBox/SearchBox';
 import Sidebar from '../Sidebar/Sidebar';
@@ -15,6 +16,107 @@ import Divider from '@mui/material/Divider';
 const drawerWidth = 240;
 
 export default function Workspace() {
+    const [notes, setNotes] = useState([]);
+    const [activeNote, setActiveNote] = useState(false);
+    const [db, setDb] = useState(null);
+
+    const [isEditing, setIsEditing] = React.useState();
+
+    const handleEditNote = () => {
+        setIsEditing(!isEditing);
+        console.log('edit');
+    };
+
+    useEffect(() => {
+        const request = indexedDB.open('notes_db', 1);
+
+        request.onerror = (event) => {
+            console.log('Database error: ', event.target.errorCode);
+        };
+
+        request.onsuccess = (event) => {
+            console.log('Database opened successfully');
+            const database = event.target.result;
+            setDb(database);
+
+            const transaction = database.transaction('notes', 'readonly');
+            const store = transaction.objectStore('notes');
+
+            const notesRequest = store.getAll();
+            notesRequest.onsuccess = () => {
+                setNotes(notesRequest.result);
+            };
+        };
+        request.onupgradeneeded = (event) => {
+            console.log('Database upgrade needed');
+            const database = event.target.result;
+            const objectStore = database.createObjectStore('notes', {
+                keyPath: 'id',
+            });
+
+            objectStore.createIndex('title', 'title', { unique: false });
+
+            console.log('Database upgrade complete');
+        };
+    }, []);
+
+    const saveNote = (note) => {
+        const transaction = db.transaction('notes', 'readwrite');
+        const store = transaction.objectStore('notes');
+        const request = store.put(note);
+
+        request.onsuccess = () => {
+            console.log('Note saved successfully');
+        };
+
+        request.onerror = () => {
+            console.log('Error saving note');
+        };
+    };
+
+    const onAddNote = () => {
+        const newNote = {
+            id: uuidv4(),
+            title: '',
+            body: '',
+            lastModified: Date.now(),
+        };
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
+        saveNote(newNote);
+    };
+    const onDeleteNote = (noteId) => {
+        const transaction = db.transaction('notes', 'readwrite');
+        const store = transaction.objectStore('notes');
+
+        const request = store.delete(noteId);
+
+        request.onsuccess = () => {
+            console.log('Note deleted successfully');
+            setNotes(notes.filter(({ id }) => id !== noteId));
+        };
+
+        request.onerror = () => {
+            console.log('Error deleting note');
+        };
+    };
+
+    const onUpdateNote = (updatedNote) => {
+        const updatedNotesArr = notes.map((note) => {
+            if (note.id === updatedNote.id) {
+                return updatedNote;
+            }
+
+            return note;
+        });
+
+        setNotes(updatedNotesArr);
+    };
+
+    const getActiveNote = () => {
+        return notes.find(({ id }) => id === activeNote);
+    };
+
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
@@ -22,7 +124,12 @@ export default function Workspace() {
                 position='fixed'
                 sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
             >
-                <SearchBox />
+                <SearchBox
+                    activeNote={activeNote}
+                    onAddNote={onAddNote}
+                    onDeleteNote={onDeleteNote}
+                    onEditNote={handleEditNote}
+                />
             </AppBar>
             <Drawer
                 variant='permanent'
@@ -38,14 +145,25 @@ export default function Workspace() {
                 <Toolbar />
                 <Box sx={{ overflow: 'auto' }}>
                     <List>
-                        <Sidebar />
+                        <Sidebar
+                            notes={notes}
+                            onDeleteNote={onDeleteNote}
+                            activeNote={activeNote}
+                            setActiveNote={setActiveNote}
+                        />
                     </List>
                     <Divider />
                 </Box>
             </Drawer>
-            <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+            <Box component='main' sx={{ flexGrow: 2, p: 3 }}>
                 <Toolbar />
-                <ListItem />
+                <ListItem
+                    activeNote={getActiveNote()}
+                    onUpdateNote={onUpdateNote}
+                    saveNote={saveNote}
+                    isEditing={isEditing}
+                    onEditNote={handleEditNote}
+                />
             </Box>
         </Box>
     );
